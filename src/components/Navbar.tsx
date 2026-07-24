54177bf63567f3995515b78405482be7465fc6ba
brycenk05 <kanobrycen@gmail.com>
2026-07-23 15:59:50 -1000

    Made starting  adjustments to landing page, navbar and footer features

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const NavBar: React.FC = () => {
   const currentUser = session?.user?.email;
   const role = session?.user?.role;
   return (
-    <Navbar bg="dark" expand="lg">
+    <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="/">Testing if changes apply to deployed site</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
