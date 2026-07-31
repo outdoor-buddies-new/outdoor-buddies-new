298732f801d82563dd1b6714aac4c1b85101b7a1
brycenk05 <kanobrycen@gmail.com>
2026-07-31 00:59:12 -1000

    Replace mockup contents with an actual HikeCard that uses trails in the db
    Also made some minor changes to HikingRecommendations and hikes\page.tsx to remove commented out user protection

--- src/app/hikes/page.tsx
+++ src/app/hikes/page.tsx
@@ -1,18 +1,22 @@
 import { loggedInProtectedPage } from '@/lib/page-protection';
 import HikeRecommendations from '@/components/HikingRecommendations';
 import { auth } from '@/lib/auth';
+import { getTrails } from '@/lib/dbActions';
 
 const HikeRecommendation = async () => {
   // Protect the page, only logged in users can access it.
-//  const session = await auth();
-//  loggedInProtectedPage(
-//    session as {
-//      user: { email: string; id: string; name: string };
-//    } | null,
-//  );
+  const session = await auth();
+  loggedInProtectedPage(
+    session as {
+      user: { email: string; id: string; name: string };
+    } | null,
+  );
+
+  const trails = await getTrails();
+
   return (
     <main>
-      <HikeRecommendations />
+      <HikeRecommendations trails={trails}/>
     </main>
   );
 };
