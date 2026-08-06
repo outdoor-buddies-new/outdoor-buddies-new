2698a8b4390e43ef7d77cc9344237939cec1ab44
KellyM12 <150403430+KellyM12@users.noreply.github.com>
2026-08-05 22:49:46 -1000

    fixing mess

--- prisma.config.ts
+++ prisma.config.ts
@@ -22,33 +22,3 @@ export default defineConfig({
     url: dbUrl,
   },
 });
-*/
-
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { defineConfig } from '@prisma/config';
-
-const connectionString = process.env.DATABASE_URL;
-
-const isLocalhost =
-  connectionString?.includes('localhost') ||
-  connectionString?.includes('127.0.0.1');
-
-const pool = new Pool({
-  connectionString,
-  ssl: isLocalhost ? false : { rejectUnauthorized: false },
-});
-
-const adapter = new PrismaPg(pool);
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-    seed: 'npx tsx prisma/seed.ts',
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"] || "",
-  },
-  adapter,
-});
